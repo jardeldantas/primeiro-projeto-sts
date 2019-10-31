d973793348393a99344ad5d8ea3bd32ffb422f4e
jardeldantas <jarde.dantas@outlook.com>
2019-10-31 00:50:44 -0300

    Arquivos iniciais do projeto

---
+++ src/test/java/com/meudominio/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
